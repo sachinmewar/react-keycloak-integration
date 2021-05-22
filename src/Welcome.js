a8a2cf77ed0b97193082fa2d81046db57dd0a921
unknown <sachinmewar800@gmail.com>
2021-05-22 18:01:11 +0530

    Added Welcome Page in Welcome.js

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+
+const Welcome = () => {
+   return (
+      <div>
+         <p> This is  a Welcome Page </p>
+         <p> Secure Component requires Authentication</p>
+      </div>
+   )
+};
+
+export default Welcome;
