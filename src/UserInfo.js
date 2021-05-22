a54373f863de3b8029833aadaa25dda14349ccf3
unknown <sachinmewar800@gmail.com>
2021-05-22 21:02:41 +0530

    Give us the authenticated user info

---
+++ src/UserInfo.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+const UserInfo = ({ keycloak }) => {
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [id, setId] = useState('');
+
+   keycloak.loadUserInfo().then(
+      userInfo => {
+         setName(userInfo.name);
+         setEmail(userInfo.email);
+         setId(userInfo.id);
+      }
+   );
+   return (
+      <div className="UserInfo">
+         <p>Name: {name}</p>
+         <p>Email: {email}</p>
+         <p>ID: {id}</p>
+      </div>
+   );
+}
+
+export default UserInfo;
